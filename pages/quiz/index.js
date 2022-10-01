62470ed809605803879d6a6469fa5fbd050a0e4d
Shams Wali Sowmo <60974766+Sowmo0509@users.noreply.github.com>
2022-10-02 01:25:32 +0600

    Update index.js

--- pages/quiz/index.js
+++ pages/quiz/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import questions from "../../data/quiz";
 import { useRouter } from "next/router";
 import PlayButton from "../../components/PlayButton";
