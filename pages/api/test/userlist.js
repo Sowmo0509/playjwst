0f8c10ec6ba7a7a52d467d2943f6f37857518231
Shams Wali Sowmo <60974766+Sowmo0509@users.noreply.github.com>
2022-10-02 00:42:01 +0600

    Update userlist.js

--- pages/api/test/userlist.js
+++ pages/api/test/userlist.js
@@ -1,7 +1,7 @@
 import connectMongo from "../../../utils/connectMongo";
 import User from "../../../models/userModel";
 
-export default async function handler(req, res) {
+export default function handler(req, res) {
   console.log("Connecting to DB...");
   await connectMongo();
   console.log("Connected!");
@@ -9,6 +9,5 @@ export default async function handler(req, res) {
 const user = await User.find();
 console.log(user);
 
-  // res.status(200).json({ user });
-  res.send("Done fetching user from api");
+  res.status(200).json({ user });
 }
