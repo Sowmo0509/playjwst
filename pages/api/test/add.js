a746ef28fcc2c0e4a79f37b9cc0dd3b2287c2450
Shams Wali Sowmo <60974766+Sowmo0509@users.noreply.github.com>
2022-10-02 00:30:28 +0600

    Add files via upload

--- pages/api/test/add.js
+++ pages/api/test/add.js
@@ -1,7 +1,7 @@
-import connectMongo from "../../../utils/connectMongo";
-import User from "../../../models/userModel";
+import connectMongo from "../../utils/connectMongo";
+import User from "../../models/userModel";
 
-export default async function addTest(req, res) {
+export default async function handler(req, res) {
   console.log("Connecting to DB...");
   await connectMongo();
   console.log("Connected!");
@@ -12,11 +12,11 @@ export default async function addTest(req, res) {
     await User.create(req.body);
   } catch (error) {
     if (error.code == "11000") {
-      res.send({ errorMsg: "Same username found. Please change username." });
+      res.status(200).send({ errorMsg: "Same username found. Please change username." });
     } else {
-      res.send({ successMsg: "User added." });
+      res.status(200).send({ successMsg: "User added." });
     }
   }
   console.log("Created!");
-  res.send("CREATED NEW PLAYER PROFILE");
+  res.status(200).send("CREATED NEW PLAYER PROFILE");
 }
