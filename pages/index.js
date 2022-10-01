b7f07508eda393f7397575116e6907d90c72f9b1
Shams Wali Sowmo <60974766+Sowmo0509@users.noreply.github.com>
2022-10-01 23:54:49 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -43,14 +43,17 @@ export default function Home({ userdata }) {
     </section>
   );
 }
-
 // Getting leaderboard data
 export async function getServerSideProps() {
-  const res = await fetch("https://playjwst.vercel.app/api/test/userlist");
-  const data = await res.json();
-  console.log(data);
+  // const res = await fetch(`${process.env.URL}/api/test/userlist`);
+  // const data = await res.json();
+  // console.log(data);
 
   return {
-    props: { userdata: data },
+    props: {
+      userdata: {
+        user: [{ username: "bla", point: "bla" }],
+      },
+    },
   };
 }
